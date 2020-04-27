91e10b25df379d3c07064718a6f279ddba3f3eb7
dominicdaviescodes <codewithdom@gmail.com>
2020-04-27 21:10:14 +0100

    add notes for 3.6

--- Ex_Files/Exercise Files/Chap03/03_06_begin.py
+++ Ex_Files/Exercise Files/Chap03/03_06_begin.py
@@ -1,12 +1,16 @@
+# comments are notes to your future self and others to describe what your code does
+# comment out code also
 
+
+# Greet the user
 print("Hi!")
 
-name = input("What's your name? ")
+name = input("What's your name? ") # asks the user their name
 print("It's nice to meet you,", name)
 
-answer = input("Are you enjoying the course? ")
+# answer = input("Are you enjoying the course? ")
 
-if answer == "Yes":
-     print("That's good to hear!")
-else:
-    print("Oh no! That makes me sad!")
+# if answer == "Yes":
+#     print("That's good to hear!")
+# else:
+#    print("Oh no! That makes me sad!")
