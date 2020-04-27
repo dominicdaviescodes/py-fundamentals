0da3f512569ce3515b7ddb97a48c584b9405ee0d
dominicdaviescodes <codewithdom@gmail.com>
2020-04-27 20:47:43 +0100

    add notes for 3.3

---
+++ Ex_Files/Exercise Files/Chap03/03_03_numbers.py
@@ -0,0 +1,24 @@
+# common operators
+
+# exponents
+
+print(2 ** 3)
+
+# floor division
+
+print(3 // 2)
+
+# modulo - if you only want the remainder
+
+print(5 % 3)
+
+# large numbers - must always use no comma's
+
+print(45000 / 5)
+
+# fractional numbers are everywhere in life and code!
+# if paid £10.85 per hour the .85 is important!
+# python has whole and decimal (floats) - more precision
+# if use 2 floats, your answer is a float
+print(0.2 / 0.3)
+# 0.6666666666666667  (we'll tweak the long floats later on)
