71c91d9fe049cba3637b3cf953975be952c95e79
dominicdaviescodes <codewithdom@gmail.com>
2020-04-27 21:04:33 +0100

    add notes for 3.5

--- Ex_Files/Exercise Files/Chap03/03_05_begin.py
+++ Ex_Files/Exercise Files/Chap03/03_05_begin.py
@@ -1,7 +1,13 @@
+"""
+Properly using whitespace makes code readable
+
+"""
 print("Hi!")
 name = input("What's your name? ")
+
 print("It's nice to meet you,", name)
 answer = input("Are you enjoying the course? ")
+
 if answer == "Yes":
     print("That's good to hear!")
 else:
