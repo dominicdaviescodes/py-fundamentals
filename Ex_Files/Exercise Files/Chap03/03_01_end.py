83c6929480cc61bdc0ad844c7b158cdee6c9e475
dominicdaviescodes <codewithdom@gmail.com>
2020-04-27 19:52:38 +0100

    add notes for 3.1

---
+++ Ex_Files/Exercise Files/Chap03/03_01_end.py
@@ -0,0 +1,20 @@
+
+"""
+Notes
+-----
+a var is a container for a value
+give a name and set (assign) a value
+= is the assignment operator
+
+"""
+# number data type. integer (whole number)
+age = 36
+print(age)
+print(type(age))  # int
+
+# string data type. a string of characters
+email_address = "john.doe@me.com"
+print(email_address)
+print(type(email_address))  # str
+
+# use type function to get the type of any value/variable
