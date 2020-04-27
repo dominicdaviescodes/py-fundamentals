2b2c2f79a95e2920012995265dc810188a4c3584
dominicdaviescodes <codewithdom@gmail.com>
2020-04-27 20:05:38 +0100

    add notes for 3.2

--- Ex_Files/Exercise Files/Chap03/03_02_end.py
+++ Ex_Files/Exercise Files/Chap03/03_02_end.py
@@ -1,3 +1,17 @@
+"""
+Strict
+some languages make you define vars and their types before you use them
+can't change type later from a string to an integer
+fewer suprises later!
+
+Python - more relaxed (flexable)
+we declare and use a var at the same time
+interpreter figures out the type based on the value we provide
+can change type later.
+
+"""
+
+# can change type later in python
 cookies = 'Sugar'
 print(cookies)
 print(type(cookies))
@@ -9,6 +23,8 @@ print(type(cookies))
 Cookies = 3
 print(Cookies)
 print(cookies)
+
+# variable names are case sensitive are 2 different locations in memory
 
 first_name = "Jeff"
 print(first_name)
