4652501ad9c4d83e92c747f35d7cbb826babe8fb
dominicdaviescodes <codewithdom@gmail.com>
2020-04-28 12:26:01 +0100

    add notes for lecture 4.1

---
+++ Ex_Files/Exercise Files/Chap04/04_01.py
@@ -0,0 +1,34 @@
+"""
+Conditional or Boolean expression :-
+Any expression that breaks down  to either true/false
+
+Relational operators:
+operand1 (operator) operand2 => True / False
+
+Equality
+operand1 (operator) operand2 => True / False
+operand1     ==     operand2 => True / False
+
+4 == 2*2 => True
+
+name = "Dominic"
+name == "Frank" => False
+
+"""
+print(5 == 4)  # False
+
+# not equals
+print(5 != 4)  # True  5 isn't same thing as 4
+
+# greater than
+
+print(5 > 4)
+
+# less than
+
+print(5 < 4)
+
+age = 15
+age_to_drive = 15
+
+print(age == age_to_drive)  # compares values of the 2 variables.
