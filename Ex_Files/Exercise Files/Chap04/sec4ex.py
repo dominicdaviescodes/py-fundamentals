162bb1e169bff1cb941863f469143ab8f23fd9b9
dominicdaviescodes <codewithdom@gmail.com>
2020-04-28 16:30:52 +0100

    add notes for section 4 exercises

---
+++ Ex_Files/Exercise Files/Chap04/sec4ex.py
@@ -0,0 +1,21 @@
+print(17 < 17)  # false
+print(15 == 3 * 5)  # true
+print(15 <= 3 * 5)  # true
+print("Frank" == "Sarah")  # false
+age = 13
+print(age != 10	)  # true
+
+age = 13
+print(15 >= age + 2	)  # true
+
+
+"""
+prints
+False
+True
+True
+False
+True
+True
+
+"""
