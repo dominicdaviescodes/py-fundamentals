0c056a635e32ba4d300c36f7a31ef9ac2d7a3e67
dominicdaviescodes <codewithdom@gmail.com>
2020-04-28 16:01:17 +0100

    add notes for lecture 4.3

--- Ex_Files/Exercise Files/Chap04/04_03_begin.py
+++ Ex_Files/Exercise Files/Chap04/04_03_begin.py
@@ -1,3 +1,20 @@
+"""
+if-else statement
+add more flexability. so far if condition is false nothing happens.
+
+if condition:
+    do work
+else:                           # else clause
+    do something else
+
+if 5 > 6:
+    print("5 is greater than 6. Weird!")  # skips this
+else:
+    print("5 is not greater than 6")
+
+"""
+
+
 plant = "Cacti"
 
 if plant == "Cacti":
@@ -5,4 +22,4 @@ if plant == "Cacti":
 else:
     print(plant, "love water")
 
-print("Thanks!")
+print("Thanks!")   # regardless of the value of the variable we always print thanks
