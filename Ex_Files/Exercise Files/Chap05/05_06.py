ea2b89e5ff1bc53d838b354f5443c8180b793e45
dominicdaviescodes <codewithdom@gmail.com>
2020-04-29 19:00:07 +0100

    add notes for lecture 5.6 and 5.7

---
+++ Ex_Files/Exercise Files/Chap05/05_06.py
@@ -0,0 +1,19 @@
+"""
+Challenge
+ write a program that prints out the name of your fave city
+
+1. have a function named favorite_city   -  done
+2. favorite_city should have one parameter: name   - done
+3. call favorite_city 3x
+4. output: "one of my favorite cities is"
+"""
+# prints out name of a favorite city
+
+
+def favorite_city(name):
+    print("one of my favorite cities is", name)
+
+
+favorite_city("London")
+favorite_city("Dallas")
+favorite_city("Ontario")
