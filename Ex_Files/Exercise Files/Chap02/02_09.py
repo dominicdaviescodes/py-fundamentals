825ac74a78f4c8074ab8b54148df99527a466c15
dominicdaviescodes <codewithdom@gmail.com>
2020-04-29 20:58:44 +0100

    edit readme

---
+++ Ex_Files/Exercise Files/Chap02/02_09.py
@@ -0,0 +1,12 @@
+# syntax error
+print("Hello world")
+
+# runtime error
+10 * (2/0)
+# division by zero is undefined, causes python to error
+
+# semantic error
+name = "Alice"
+print("Hello name")
+
+print("Hello ${name}")
