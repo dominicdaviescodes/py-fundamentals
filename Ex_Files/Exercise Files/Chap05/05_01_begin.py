e7e79bd038341bc24a477d1d1cdc685bedeb76ce
dominicdaviescodes <codewithdom@gmail.com>
2020-04-29 16:37:50 +0100

    add notes for lecture 5.1

---
+++ Ex_Files/Exercise Files/Chap05/05_01_begin.py
@@ -0,0 +1,22 @@
+# Function: block of code packaged together with a name, a way to break up your code
+
+# already used 2
+# print()
+# input()
+
+# package instructions 1 time, then call whenever you need that job done
+# in the end file we create a function and call it 3 times rather than copy/paste steps 3 x
+
+print("~~~ The Shimmy ~~~")
+
+print("Take one step to the right and stomp.")
+print("Take one step to the left and stomp.")
+print("Shake those hips!")
+
+print("Take one step to the right and stomp.")
+print("Take one step to the left and stomp.")
+print("Shake those hips!")
+
+print("Take one step to the right and stomp.")
+print("Take one step to the left and stomp.")
+print("Shake those hips!")
