b8c6649580690e44c393ae0051371712aaa056f3
dominicdaviescodes <codewithdom@gmail.com>
2020-04-29 16:46:18 +0100

    add notes for lecture 5.2

--- Ex_Files/Exercise Files/Chap05/05_02_begin.py
+++ Ex_Files/Exercise Files/Chap05/05_02_begin.py
@@ -1,5 +1,17 @@
+# def lets python know we are creating a function
+
+"""
+def unique_name():
+    print("this code runs when called")
+
+# call it!
+unique_name()
+"""
+
+
 def say_hello():
     print("Hello, friends!")
+
 
 say_hello()
 say_hello()
