ae2490c265785319e708a72614efcbbc40d5aa75
dominicdaviescodes <codewithdom@gmail.com>
2020-04-29 17:40:55 +0100

    add notes for lecture 5.3

--- Ex_Files/Exercise Files/Chap05/05_03_begin.py
+++ Ex_Files/Exercise Files/Chap05/05_03_begin.py
@@ -1,8 +1,22 @@
-def wash_car():
-    print("Wash with tri-color foam")
-    print("Rinse twice")
-    print("Dry with large blow dryer")
+# provide different outputs for some users based on input
 
-    print("Wash with white foam")
-    print("Rinse once")
-    print("Air dry") 
+# amount_paid here is a parameter
+
+
+def wash_car(amount_paid):
+    if (amount_paid == 12):
+        # steps for platinum wash $12
+        print("Wash with tri-color foam")
+        print("Rinse twice")
+        print("Dry with large blow dryer")
+
+    if (amount_paid == 6):
+        # steps for basic wash $6
+        print("Wash with white foam")
+        print("Rinse once")
+        print("Air dry")
+
+
+# the values we give to our functions are the arguments - in the function call
+# function_call(argument)
+wash_car(12)
