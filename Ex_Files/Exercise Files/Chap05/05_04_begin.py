0bc27e1fb57d550834dd7327c4da6d6a66ace547
dominicdaviescodes <codewithdom@gmail.com>
2020-04-29 20:43:32 +0100

    edit notes for 5.4

--- Ex_Files/Exercise Files/Chap05/05_04_begin.py
+++ Ex_Files/Exercise Files/Chap05/05_04_begin.py
@@ -1,10 +1,11 @@
 # Return a value.  When we need something back
+# keyword is used to send back values from a Python function.
+
 def withdraw_money(current_balance, amount):
     if (current_balance >= amount):
         current_balance = current_balance - amount
         print("The balance is", current_balance)
 
+
 withdraw_money(100, 80)  # 20
-
-
 
